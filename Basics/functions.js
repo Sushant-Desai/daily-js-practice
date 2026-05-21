025b38c3734762376d8d12d4723121606494911f
Sushant-Desai <161923974+Sushant-Desai@users.noreply.github.com>
2026-05-21 11:37:05 +0530

    functions and various types of fuction in js

--- Basics/functions.js
+++ Basics/functions.js
@@ -47,9 +47,51 @@ function largestNum(num1, num2) {
     } else if (num2 > num1) {
         console.log(num2 + " is Greater than" + num1);
     } else {
-        console.log("Number "+num1 ,num2 + " Both Numbers are equal")
+        console.log("Number " + num1, num2 + " Both Numbers are equal")
     }
 }
-let result=largestNum(25,25)
+let result = largestNum(25, 25)
 console.log(result);
-console.log(largestNum(105,801));
+console.log(largestNum(105, 801));
+
+
+/* Functions - A block of code  used to perform specific task */
+// basic function syntax
+
+greet();
+
+// Function Expression - A function Assigned to a varibale
+const welcome = function greet() {
+    console.log("Hello Sushant");
+}
+
+//Arrow Function (ES6) - Short syntax for writing functions.
+
+const mul = (a, b) => {
+    return a * b;
+}
+console.log(mul(10, 20));
+
+
+// CAllBACK Function - A function passed as an argument to another function.
+
+function greet2(name, callback, s) {
+    console.log("Hello " + name);
+    callback();
+    s();
+}
+function bye() {
+    console.log("Bye");
+}
+function seeU() {
+    console.log(" See U again ");
+}
+
+greet2("Sushant", bye, seeU);
+
+
+/*Anonymous Function - Function without a name. */
+
+setTimeout(function () {
+    console.log("Hello from Sushant ");
+});
